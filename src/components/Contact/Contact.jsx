bd8975ec05a63cf39a99ccd267cbe82e7b14786a
Ezequiel Weiss <38471904+ezeweiss@users.noreply.github.com>
2025-02-04 09:43:43 -0300

    cambio url

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -26,7 +26,7 @@ const Contact = () => {
   
     // Enviar los datos al backend
     try {
-      const response = await fetch('http://localhost:5000/send-email', {
+      const response = await fetch('https://backend-fw-developers.vercel.app/api/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
